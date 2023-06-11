b70a3d6217f73cd63d29bd6005de7a33e425e3d5
Rajendra Singh Rathore <75214295+RRajendrasingh@users.noreply.github.com>
2023-06-11 23:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-});
+//   document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
 
-document.addEventListener('selectstart', function (e) {
-  e.preventDefault();
-});
+// document.addEventListener('selectstart', function (e) {
+//   e.preventDefault();
+// });
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === "F12") {
-    e.preventDefault();
-  }
-});
+// document.addEventListener('keydown', function(e) {
+//   if (e.key === "F12") {
+//     e.preventDefault();
+//   }
+// });
 
 
 
